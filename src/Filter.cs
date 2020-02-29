5439440fb746e3b37c703b9a8cd220955b0f1e41
Andrew Sorensen <andrew@moso.com.au>
2020-02-29 16:40:34 +1100

    init filter

---
+++ src/Filter.cs
@@ -0,0 +1,103 @@
+﻿/***
+
+THE DISUNITY SYNTHESIZER TOOLKIT
+
+Copyright 2020 Andrew Sorensen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+***/
+
+using UnityEngine;
+using System;
+
+namespace DST {
+    public class Filter : AudioUnit
+    {
+        public enum FilterType {
+            LOW,
+            HIGH,
+            BAND,
+            NOTCH,
+            PEAK,
+            ALLPASS
+        }
+
+        public AudioUnit input;
+        public AudioUnit cutoff;
+        [Range(0.0F, 0.995F)]
+        public double resonance = 0.1;
+        public FilterType filterType = FilterType.LOW;
+
+        private double g = 0.0;
+        private double k = 0.0;
+        private double a1 = 0.0;
+        private double a2 = 0.0;
+        private double v1 = 0.0;
+        private double v2 = 0.0;
+        private double ic1eq = 0.0;
+        private double ic2eq = 0.0;
+
+        private float[] cutoffData;
+        private double oldCutFrq = -1.0;
+        private double oldRes = -1.0;
+        private double SampleRate; 
+
+        void Start() {
+            SampleRate = AudioSettings.outputSampleRate;
+            int buflength, numbufs;
+            AudioSettings.GetDSPBufferSize(out buflength, out numbufs);
+            var channels = AudioUnit.speakerModeToChannels(AudioSettings.speakerMode);
+            cutoffData = new float[buflength * channels];
+        }
+
+        public override void ProcessAudio(float[] data, long sampleNum, int channels) {
+            if (cutoff != null) {
+                cutoff.ProcessAudio(cutoffData, sampleNum, channels);
+            }
+            if (input == null) { throw new Exception("Must define an AudioInput"); }
+            input.ProcessAudio(data, sampleNum, channels);
+
+            for (int i=0;i<data.Length;i++ ) {
+                var cutFrq = cutoff == null ? 1000.0 : (double) cutoffData[i];
+                if (oldCutFrq != cutFrq || oldRes != resonance) {
+                    oldCutFrq = cutFrq;
+                    oldRes = resonance;
+                    g = Math.Tan(3.141592 * (cutFrq / SampleRate));
+                    k = 2.0 - (2.0 * resonance);
+                    a1 = (1.0 / (1.0 + (g * (g + k))));
+                    a2 = g * a1;
+                }
+                v1 = (a1 * ic1eq) + (a2 * (data[i] - ic2eq));
+                v2 = ic2eq + (g * v1);
+                ic1eq = (2.0 * v1) - ic1eq;
+                ic2eq = (2.0 * v2) - ic2eq;
+                switch (filterType) {
+                    case FilterType.LOW: { data[i] = (float) v2; break; }
+                    case FilterType.BAND: { data[i] = (float) v1; break; }
+                    case FilterType.HIGH: { 
+                        data[i] = (float) ((data[i] - (k * v1)) - v2);
+                        break;
+                    }
+                    case FilterType.NOTCH: {
+                        data[i] = (float) (data[i] - (k * v1));
+                        break;
+                    }
+                    case FilterType.PEAK: {
+                        data[i] = (float) ((data[i] - (k * v1)) - (2.0 * v2));
+                        break;
+                    }
+                    case FilterType.ALLPASS: {
+                        data[i] = (float) (data[i] - (2.0 * k * v1));
+                        break;
+                    }
+                }
+            }
+            return;
+        } 
+    }
+}
